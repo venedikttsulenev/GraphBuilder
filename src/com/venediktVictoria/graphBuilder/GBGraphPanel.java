a011e85688df83ac7eeb1d71158f9c4edd0a49d2
Ermoshkina Victoria <ezhevika.96@gmail.com>
2015-12-14 00:02:33 +0300

    Добавлена отрисовка графика

---
+++ src/com/venediktVictoria/graphBuilder/GBGraphPanel.java
@@ -0,0 +1,175 @@
+package com.venediktVictoria.graphBuilder;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
+import java.awt.Cursor;
+
+import javax.swing.JPanel;
+
+//import com.sun.javafx.cursor.*;
+
+import sun.security.util.SecurityConstants.AWT;
+
+public class GBGraphPanel extends JPanel {
+	private static double MAX_GRAPH_BOUNDS_DIFF = 1000000000;
+	private static double MIN_GRAPH_BOUNDS_DIFF = 0.0001; 
+	private static double INITIAL_LEFT_GRAPH_BOUND = -5;
+	private static double INITIAL_RIGHT_GRAPH_BOUND = 5;
+	
+	private GBFunction func_;
+	private double leftGraphBound_, rightGraphBound_;
+	private double bottomGraphBound_, topGraphBound_;
+	private Point2D.Double mid_;
+	private Point2D.Double diff_;
+	private boolean mouseDragging_;
+	private Point dragStart_;
+	/*
+	private Point2D.Double pixToCoords(Point p) {
+		return new Point2D.Double(mid_.x + 0.001*(p.x - getWidth()/2)*diff_.x/getWidth(), mid_.y + 0.001*(p.y - getHeight()/2)*diff_.y/getHeight());
+	}*/
+	private Point coordsToPix(double x, double y) {
+		return new Point(getWidth()/2 + (int)((x - mid_.x)*((double)getWidth()/diff_.x)) , getHeight()/2 - (int)((y - mid_.y)*((double)getHeight()/diff_.y)));
+	}
+	
+	public GBGraphPanel(int width, int height) {
+		super();
+		func_ = null;
+		leftGraphBound_ = INITIAL_LEFT_GRAPH_BOUND;
+		rightGraphBound_ = INITIAL_RIGHT_GRAPH_BOUND;
+		diff_ = new Point2D.Double(10, 10*width/height);
+		mid_ = new Point2D.Double(0, 0);
+		bottomGraphBound_ = mid_.y - diff_.y/2;
+		topGraphBound_ = mid_.y + diff_.y/2;
+		mouseDragging_ = false;
+		dragStart_ = new Point(0, 0);
+		setCursor(new Cursor(Cursor.HAND_CURSOR));
+		addMouseListener(new MouseListener() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseDragging_ = true;
+				dragStart_.x = e.getX();
+				dragStart_.y = getHeight() - e.getY();
+				
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (mouseDragging_ == true) {
+					mouseDragging_ = false;
+					Point p = e.getPoint();
+					p.setLocation(p.getX(), getHeight() - p.y);
+					System.out.println(p);
+					Point2D.Double newMid = new Point2D.Double(mid_.x - diff_.x*(p.x - dragStart_.x)/(double)getWidth(), mid_.y - diff_.y*(p.y - dragStart_.y)/(double)getHeight());
+					setMid(newMid);
+					repaint();
+				}
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			@Override
+			public void mouseClicked(MouseEvent e) {}
+		});
+		addMouseMotionListener(new MouseMotionListener() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				
+			}
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				
+				
+			}
+		});
+	}
+	public void setGraphBounds(Double left, Double right) {
+		if (left == null && right == null) {
+			leftGraphBound_ = INITIAL_LEFT_GRAPH_BOUND;
+			rightGraphBound_ = INITIAL_RIGHT_GRAPH_BOUND;
+			mid_.x = 0;
+		}
+		else if (right == null) {
+			leftGraphBound_ = left.doubleValue() - 5;
+			rightGraphBound_ = left.doubleValue() + 5;
+			mid_.x = left.doubleValue();
+		}
+		else {
+			leftGraphBound_ = Math.min(left.doubleValue(), right.doubleValue());
+			rightGraphBound_ = Math.max(left.doubleValue(), right.doubleValue());
+			mid_.x = (leftGraphBound_ + rightGraphBound_) / 2;
+			diff_.x = rightGraphBound_ - leftGraphBound_;
+			if (diff_.x > MAX_GRAPH_BOUNDS_DIFF || diff_.x < MIN_GRAPH_BOUNDS_DIFF) {
+				double scale = (diff_.x > MAX_GRAPH_BOUNDS_DIFF) ? MAX_GRAPH_BOUNDS_DIFF / diff_.x : MIN_GRAPH_BOUNDS_DIFF / diff_.x;
+				leftGraphBound_ = mid_.x + scale*(leftGraphBound_ - mid_.x);
+				rightGraphBound_ = mid_.x + scale*(rightGraphBound_ - mid_.x);
+				diff_.x = rightGraphBound_ - leftGraphBound_; //for correct calculation of bottom and top bounds
+			}
+		}
+		mid_.y = 0;
+		diff_.y = diff_.x*getHeight() / getWidth();
+		bottomGraphBound_ = mid_.y - diff_.y/2;
+		topGraphBound_ = mid_.y + diff_.y/2;
+	}
+	public void setMid(Point2D.Double p) {
+		mid_.x = p.x;
+		mid_.y = p.y;
+		leftGraphBound_ = mid_.x - diff_.x/2;
+		rightGraphBound_ = leftGraphBound_ + diff_.x;
+		bottomGraphBound_ = mid_.y - diff_.y/2;
+		topGraphBound_ = bottomGraphBound_ + diff_.y;
+	}
+	public void setFunction(GBFunction func) { 
+		func_ = func;
+	}
+//----------------------------------------	
+	public double getLeftGraphBound() {
+		return leftGraphBound_;
+	}
+	public double getRightGraphBound() {
+		return rightGraphBound_;
+	}
+	public double getTopGraphBound() {
+		return topGraphBound_;
+	}
+	public double getBottomGraphBound() {
+		return bottomGraphBound_;
+	}
+//----------------------------------------	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
+		g2d.drawLine(0, 0, getWidth(), 0);
+		g2d.setStroke(new BasicStroke(2));
+		if (bottomGraphBound_ < 0 && topGraphBound_ > 0) {
+			Point xAxisBeg = coordsToPix(leftGraphBound_, 0);
+			Point xAxisEnd = coordsToPix(rightGraphBound_, 0);
+			g2d.drawLine(xAxisBeg.x, xAxisBeg.y, xAxisEnd.x, xAxisEnd.y); 	//draw X-axis
+		}	
+		if (leftGraphBound_ < 0 && rightGraphBound_ > 0) {
+			Point yAxisBeg = coordsToPix(0, bottomGraphBound_);
+			Point yAxisEnd = coordsToPix(0, topGraphBound_);
+			g2d.drawLine(yAxisBeg.x, yAxisBeg.y, yAxisEnd.x, yAxisEnd.y);	//draw Y-axis
+		}
+		g2d.setStroke(new BasicStroke(1));
+		if (func_ != null) {
+			final double step = 2 * diff_.x / getWidth();
+			g.setColor(Color.BLACK);
+			for (double d = leftGraphBound_; d < rightGraphBound_; d += step) {
+				if (!func_.isSuspiciousForHavingVerticalAsymptoteIn(d, d + step) && !func_.hasVerticalAsymptoteIn(d, d + step)) {
+					Point beg = coordsToPix(d, func_.value(d));
+					Point end = coordsToPix(d + step, func_.value(d+step));
+					g.drawLine(beg.x, beg.y, end.x, end.y);
+				}
+			}
+		}
+	}
+}
--- src/com/venediktVictoria/graphBuilder/GBMainFrame.java
+++ src/com/venediktVictoria/graphBuilder/GBMainFrame.java
@@ -1,77 +1,135 @@
 package com.venediktVictoria.graphBuilder;
 
-import java.awt.event.*;
-
+//import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
 import com.venediktVictoria.graphBuilder.exceptions.GBParseException;
 
 public class GBMainFrame extends JFrame {
+	private static final int DEFAULT_MIN_WIDTH = 300;
+	private static final int DRFAULT_MIN_HEIGHT = 200;
+	private static final int DEFAULT_GAP = 10;
+	private static final int DEFAULT_CONTROL_PANEL_HEIGHT = 85;
+	private static final Color DEFAULT_CONTROL_PANEL_COLOR = new Color(205, 205, 205);
+	private static final Color DEFAULT_GRAPH_PANEL_COLOR = new Color(255, 255, 255);
+
+	private int columnWidth_;		//Width of column in JTextField (in pixels)
 	private JTextField textField_;
-	private JButton buttonBuild_;
+	private JButton buildButton_;
 	private JLabel labelFX_;
 	private JLabel errLabel_;
+	private JLabel labelA_, labelB_;
+	private JTextField textFieldA_, textFieldB_;
 	private JPanel controlPanel_;
-	
-	private void realignItems() {
-		textField_.setSize(getWidth() - 60, 26);
-		buttonBuild_.setLocation(getWidth() - 60, 46);
-		errLabel_.setSize(getWidth() - 20 - buttonBuild_.getWidth(), 20);
-	}
-	private void buildGraph() {
-		GBFunction func = null;
-		try {
-			func = new GBFunction(textField_.getText()); //Parse text field
-			errLabel_.setText("f(0) = " + String.valueOf(func.value(0)) + "     f(1) = " + String.valueOf(func.value(1))/* + "     f(" + String.valueOf(func.bisect(-1, 1, 0.00000001)) + ") = 0"*/);
-		}
-		catch (GBParseException exc) {		
-			errLabel_.setText(exc.getMessage()); //Output error message to label
-			if (!textField_.hasFocus()) 		 //Select text field for mistake correction
-				textField_.grabFocus();
-			if (exc.token() != null)
-				textField_.setCaretPosition(exc.token().pos() + exc.token().length()); //Move cursor in text field to position, where parse error occured
-		}
-		if (func != null) { //Expression given in text field is correct
-			/* Build graph...
-			 * Use func.value(x) to obtain function value at x
-			 * ...
-			 * */
-			
-		}
-	}
+	private GBGraphPanel graphPanel_;
+
 	public GBMainFrame(int width, int height, String title) {
-		setSize(width, height);
-		setLocationRelativeTo(null);
+		setPreferredSize(new Dimension(width, height));
+		setMinimumSize(new Dimension(DEFAULT_MIN_WIDTH, DRFAULT_MIN_HEIGHT));
 		setTitle(title);
 		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		textField_ = new JTextField();
-		textField_.setSize(width - 60, 26);
-		textField_.setLocation(50, 10);
-		textField_.setVisible(true);
-		textField_.addKeyListener(new KeyListener() {
+		controlPanel_ = new JPanel();
+		controlPanel_.setBackground(DEFAULT_CONTROL_PANEL_COLOR);
+		controlPanel_.setSize(getWidth(), DEFAULT_CONTROL_PANEL_HEIGHT);
+		graphPanel_ = new GBGraphPanel(getWidth(), getHeight() - controlPanel_.getHeight());
+		//graphPanel_.setSize(getWidth(), getHeight() - controlPanel_.getHeight());
+		graphPanel_.setLocation(0, DEFAULT_CONTROL_PANEL_HEIGHT);
+		graphPanel_.setBackground(DEFAULT_GRAPH_PANEL_COLOR);
+		labelFX_ = new JLabel("f(x) = ");
+		textField_ = new JTextField(50);
+		buildButton_ = new JButton("Build");
+		errLabel_ = new JLabel();
+		labelA_ = new JLabel("from");
+		textFieldA_ = new JTextField(4);
+		labelB_ = new JLabel("to");
+		textFieldB_ = new JTextField(4);
+		GroupLayout layout = new GroupLayout(controlPanel_);
+		layout.setAutoCreateGaps(true);
+		layout.setHorizontalGroup(
+			layout.createParallelGroup()
+				.addGroup(layout.createSequentialGroup()
+					.addGap(DEFAULT_GAP)
+					.addComponent(labelFX_)
+					.addComponent(textField_, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(buildButton_)
+					.addGap(DEFAULT_GAP)
+				)
+				.addGroup(layout.createSequentialGroup()
+					.addGap(DEFAULT_GAP)
+					.addComponent(errLabel_, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(labelA_)
+					.addComponent(textFieldA_, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelB_)
+					.addComponent(textFieldB_, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(DEFAULT_GAP)
+				)
+		);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addGap(DEFAULT_GAP)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(labelFX_)
+					.addComponent(textField_, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(buildButton_)
+				)
+				.addGap(DEFAULT_GAP)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(errLabel_)
+					.addComponent(labelA_)
+					.addComponent(textFieldA_, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelB_)
+					.addComponent(textFieldB_, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				) 
+				.addGap(DEFAULT_GAP)
+		);
+		controlPanel_.setLayout(layout);
+		add(controlPanel_);
+		add(graphPanel_);
+		setVisible(true);
+		pack();
+		setLocationRelativeTo(null);
+		columnWidth_ = textField_.getWidth() / textField_.getColumns();
+		if (columnWidth_ == 0)
+			columnWidth_ = 5;
+		
+		KeyListener textFieldKeyListener = new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				if (e.getKeyChar() == '\n') //Build graph if 'Enter' key pressed 
-					buildGraph();
+				if (e.getKeyChar() == '\n') { //Build graph if 'Enter' key pressed 
+					obtainGraphData(); 
+					graphPanel_.repaint();
+				}
 			}
 			@Override
 			public void keyPressed(KeyEvent e) {}
 			@Override
 			public void keyReleased(KeyEvent e) {}
-		});
-		buttonBuild_ = new JButton("Build");
-		buttonBuild_.setSize(50, 20);
-		buttonBuild_.setLocation(width - 60, 46);
-		buttonBuild_.setVisible(true);
-		buttonBuild_.addMouseListener(new MouseListener() {
+		};
+		textField_.addKeyListener(textFieldKeyListener);
+		textFieldA_.addKeyListener(textFieldKeyListener);
+		textFieldB_.addKeyListener(textFieldKeyListener);
+		
+		buildButton_.addMouseListener(new MouseListener() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				buildGraph();
+			public void mouseClicked(MouseEvent e) { //Build graph on mouse click
+				obtainGraphData(); 
+				graphPanel_.repaint();
+				graphPanel_.grabFocus();
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {}
@@ -82,18 +140,6 @@ public class GBMainFrame extends JFrame {
 			@Override
 			public void mouseExited(MouseEvent e) {}
 		});
-		labelFX_ = new JLabel("f(x) = ");
-		labelFX_.setLocation(10, 12);
-		labelFX_.setSize(40, 20);
-		labelFX_.setVisible(true);
-		errLabel_ = new JLabel();
-		errLabel_.setSize(width - 20 - buttonBuild_.getWidth(), 20);
-		errLabel_.setLocation(10, 46);
-		add(textField_);
-		add(buttonBuild_);
-		add(labelFX_);
-		add(errLabel_);
-		setVisible(true);
 		addComponentListener(new ComponentListener() {
 			@Override
 			public void componentResized(ComponentEvent e) {
@@ -106,5 +152,44 @@ public class GBMainFrame extends JFrame {
 			@Override
 			public void componentHidden(ComponentEvent e) {}
 		});
+	}
+	private void realignItems() {
+		controlPanel_.setSize(getContentPane().getWidth(), DEFAULT_CONTROL_PANEL_HEIGHT);
+		graphPanel_.setSize(getContentPane().getWidth(), getContentPane().getHeight() - controlPanel_.getHeight());
+	}
+	private void obtainGraphData() {
+		GBFunction func = null;
+		Double a = null, b = null;
+		try {
+			errLabel_.setText("");
+			func = new GBFunction(textField_.getText()); //Parse text field
+			a = new Double(textFieldA_.getText());
+			try {
+				b = new Double(textFieldB_.getText());
+			}
+			catch (NumberFormatException exc) {
+				if (textFieldB_.getText().length() > 0) {
+					errLabel_.setText("Incorrect numeric input");
+					if (!textFieldB_.hasFocus())
+						textFieldB_.grabFocus();
+				}
+			}
+		}
+		catch (NumberFormatException exc) {
+			if (textFieldA_.getText().length() > 0) {
+				errLabel_.setText("Incorrect numeric input");
+				if (!textFieldA_.hasFocus())
+					textFieldA_.grabFocus();
+			}
+		}
+		catch (GBParseException exc) {	
+			errLabel_.setText(exc.getMessage()); //Output error message to label
+			if (!textField_.hasFocus()) 		 //Select text field for mistake correction
+				textField_.grabFocus();
+			if (exc.token() != null)
+				textField_.setCaretPosition(exc.token().pos() + exc.token().length()); //Move cursor in text field to position, where parse error occured
+		}
+		graphPanel_.setFunction(func);
+		graphPanel_.setGraphBounds(a, b);
 	}
 }
